3f446bbc7cfc9a090a30a2930f0c69e2dbe09e15
jimender2 <35303639+jimender2@users.noreply.github.com>
2019-05-05 09:22:33 -0400

    Edit ConwayGameOfLife class

--- src/ConwayGameOfLife.java
+++ src/ConwayGameOfLife.java
@@ -2,7 +2,17 @@
 public class ConwayGameOfLife {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[][] X = new int[10][10];
+
+		for(int i = 0; i < X.length; i++)
+			for(int q = 0; q < X[i].length; q++)
+				X[i][q] = 1;
+
+		for(int i = 0; i < X.length; i++) {
+			for(int q = 0; q < X[i].length; q++)
+				System.out.print(X[i][q]);
+			System.out.println();
+		}
 
 	}
 
