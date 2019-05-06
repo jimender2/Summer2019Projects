2a15092fd647aa21acc093bf0fe7c5da840143fd
jimender2 <35303639+jimender2@users.noreply.github.com>
2019-05-05 22:18:14 -0400

    Added print method and random generation

--- src/ConwayGameOfLife.java
+++ src/ConwayGameOfLife.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 
 public class ConwayGameOfLife {
 
@@ -6,14 +7,41 @@ public class ConwayGameOfLife {
 
 		for(int i = 0; i < X.length; i++)
 			for(int q = 0; q < X[i].length; q++)
-				X[i][q] = 1;
+				X[i][q] = rand();
 
-		for(int i = 0; i < X.length; i++) {
-			for(int q = 0; q < X[i].length; q++)
-				System.out.print(X[i][q]);
-			System.out.println();
+		print(X);
+
+		for(int count = 10; count > 0; count--) {
+			int[][] old;
+			old = X;
+
+			int[][] N = new int[old.length][old[0].length];
+
+			// Check and change each cell
+
+
+
+
+
+			// Set new array equal to the main array
+			X = N;
 		}
+
+
 
 	}
 
+	public static int rand() {
+
+		Random rand = new Random();
+		return rand.nextInt(2);
+	}
+
+	public static void print(int[][] X) {
+		for(int i = 0; i < X.length; i++) {
+			for(int q = 0; q < X[i].length; q++)
+				System.out.print(X[i][q] + " ");
+			System.out.println();
+		}
+	}
 }
